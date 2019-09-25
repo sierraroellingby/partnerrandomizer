d89f74b46f2d075355700a46175117f18183d913
Toni Anguiano <toni.anguiano@sjsu.edu>
2019-09-24 17:32:16 -0700

    fixed

--- sketch.js
+++ sketch.js
@@ -1,161 +1,191 @@
-let Nosesniffle;
-let mic;
-let snowy = [];
+let roster = [{
+                firstName:"Zainib",
+                lastName:"Ahmed",
+                color:"Blue",
+                animal:"Giraffe",
+                book:"The Book Thief",
+                movie:"Amelie",
+                game:"none",
+                superhero:"The Hulk",
+                randomFact:"I love to eat olives"},
+               {
+                firstName: "Toni",
+                lastName: "Anguiano",
+                color: "blue",
+                animal: "cat",
+                book: "The Bell Jar",
+                movieGenre: "Horror",
+                superHero: "batman",
+                fact: "I like to hangout in cemeteries and research dark California history",
+             }, {
+               firstName: "Camila",
+                lastName: "Avina Garcia",
+                color: "Green",
+                animal:"Dog",
+                book:"Match",
+                movie:"Lady and the Tramp",
+                game:" Guitar Hero",
+                superhero:"Spiderman",
+                randomFact:"I have a twin",},
+                {
+                firstName:"Eddson",
+                lastName:"Jose",
+                color:"midnight blue",
+                animal:"pig",
+                book:"The Dark Forest",
+                movie:"The Fountain",
+                game:"Halo Reach",
+                superhero:"Superman",
+                fact:"I am made up of stardust",},
+                {
+                firstName: "Lydia",
+                lastName: "Lim",
+                color: "blue",
+                animal : "dog",
+                book: "No",
+                movie: "me before you",
+                game: "No",
+                superhero: "Thor",
+                randomFact: " birds are tetrapods even though they only walk on two legs",},
+                {
+                firstName:"Chi",
+                lastName:"Nguyen",
+                color:"purple",
+                animal:"bird",
+                book:"Math Curse",
+                movie:"Big Hero 6",
+                game:"Checkers",
+                superhero:"Big Hero 6 (team)",
+                randomFact:"Lately this year, monsters have especially been occupying my mind, among other things."},
+                {
+                firstName: "Leon",
+                lastName: "Quach",
+                color: "silver",
+                animal:"penguins",
+                book: "Letters to a Young Poet",
+                movie:"Kung fu Hustle",
+                game: "Pokemon Crystal Version",
+                superhero: "Batman",
+                randomFact:"I have two pet guinea pigs"},
+                {
+                firstName: "Sierra",
+                lastName: "Roe",
+                color: "purple",
+                animal: "dog",
+                book: "phantom tollbooth",
+                movie: "Naussica of the Valley of the Wind",
+                game: "Pokemon heartgold",
+                superHero: "Captain Marvel",
+                randomFact: "can run 7 minute mile"},
+                {
+                firstName: "Hao",
+                lastName: "Rong",
+                favoriteColor: "hex(#4285F4)",
+                animal: "otter",
+                book: "A Brief History of Time",
+                movie: "Angry Birds 2",
+                game: "Final Fantasy XIV",
+                superhero: "Tony Stark",
+                randomFact: "I like cats.",},
+                {
+                firstname  : "Kaicong",
+                lastname: "Tang",
+                color: "black",
+                animal: "dog",
+                movie: "iron man123",
+                game: "csgo",
+                superhero: "iron man",
+                fact: "love gaming"},
+                {
+                firstName: "Sumedh",
+                lastName: "Vedanthi",
+                color:"Blue" ,
+                animal: "Crow",
+                book: "Of Mice and Men",
+                movie: "American Psycho",
+                game: "Fruit Ninja" ,
+                superHero: "Batman" ,
+                randomFact: "im a black belt",},
+                {
+                firstName: "Chris",
+                lastName: "Weber",
+                color: "green",
+                animal: "cat",
+                book: "Hitchhiker's Guide to the Galaxy",
+                movie: "Spiderman Into The Spiderverse",
+                game: "The Legend of Zelda Breath of the Wild",
+                superHero: "Iron Man",
+                randomFact: "I'm vegetarian"},
+                {
+                firstName: "Andy",
+                lastName: "Wu",
+                color: "Red",
+                animal:"Kiwi",
+                book: "Romance of the Three Kingdoms",
+                movie:"Shrek 2",
+                game: "Super Mario 64",
+                superhero: "Static Shock",
+                randomFact:"I've had jaw correction surgery"},
+                {
+                firstName: "xiaowei",
+                lastName: "zheng",
+                color:" green",
+                animal: "babies",
+                book: "the little girl at the window",
+                movie: "eat drink man woman",
+                game: "none",
+                superHero: "none",
+                randomFact: "don't drink coke"},
+                {
+                firstName: "Matteo",
+                lastName: "Leva",
+                color: "Black",
+                animal: "Cat",
+                book: "Pillars of the earth",
+                movie:"The Godfather",
+                game: "Witcher 3",
+                superHero: "spider man",
+                fact: "I'm disappointed in GOT season 8"},
+                {
+                firstName: "Hector",
+                lastName: "Flores",
+                color: "Blue",
+                animal: "Dog",
+                boook: "Captain Underpants",
+                movie: "Ace Ventura pet detective",
+                game: "Smash Brothers",
+                superhero: "Flash",
+                randomFact: "I am a Libra"},
+                {
+                firstName: "Vanessa",
+                lastName: "Blacow-Berggen",
+                color: "pink",
+                animal:"cat",
+                book:"The Heartstriker series",
+                movie:"Ace Ventura pet detective",
+                game:"Smash Brothers",
+                superHero:"Spiderman",
+                randomFact:"I really like cats, especially my cat"},
+                {
+                firstName:"Kevin",
+                lastName: "Sagara",
+                color:"yellow",
+                animal:"cat",
+                book:"sherlock holmes",
+                movie:"Shaun of the Dead",
+                game:"osu!",
+                superhero:"batman",
+                fact:"learning japanese",}];
 
 
-function setup() {
+function setup(){
+  createCanvas(600, 600);
+  background(200);
 
-  createCanvas(400, 400);
-  angleMode(DEGREES)
-
-  for (let i = 0; i <= 10; i++) {
-    snowy[i] = new Snow(random(1, 10) / 10 * 50 * i, 5 + i * 10, .6 + i * .3);
-  }
-
-  //snowy[0] = new Snow(270, 60, 0.8);
-  //snowy[1] = new Snow(20, 300, 1.2);
-  //snowy[2] = new Snow(230, 80, 0.5);
-  //snowy[3] = new Snow(60, 275, 1);
-  //snowy[4] = new Snow(350, 100, 1.5);
-
-
-
-  mic = new p5.AudioIn()
-  mic.start();
+console.log(roster[1]);
 }
 
-function draw() {
-
-  background(250, 170, 80);
-
-  Nosesniffle = map(mic.getLevel(), 0, .5, 169, 210);
+function draw(){
 
 
-
-  if (mouseIsPressed && mouseX <= width * 0.6 && mouseX >= width * 0.4 && mouseY <= height * 0.85 && mouseY >= height * 0.7) {
-
-    background(0)
-    console.log("hi")
-  }
-
-  if (mouseX > width * 0.55 && mouseX < width * 0.42 && mouseY < height * 0.8 && mouseY > height * 0.7) {
-    background(250, 170, 80);
-
-  }
-
-
-
-  drawHead();
-
-  drawEyes();
-
-  drawRightpupil();
-
-  drawLeftpupil();
-
-  drawNose();
-
-  drawNosedot();
-
-
-  for (i = 0; i < snowy.length - 1; i++) {
-    snowy[i].display();
-    snowy[i].move();
-  }
-  //snowy[0].display();
-  //snowy[1].display();
-  // snowy[2].display();
-  //snowy[3].display();
-  //snowy[4].display();
-
-  //snowy[0].move();
-  //snowy[1].move();
-  //snowy[2].move();
-  //snowy[3].move();
-  //snowy[4].move();
-
-  console.log(snowy)
-}
-
-//Functions
-
-
-function drawLeftpupil() {
-  stroke(color(145, 100, 80))
-  strokeWeight(4)
-  fill(170, 100, 80)
-  circle(width * .37, height * .35, 17);
-}
-
-function drawEyes() {
-  fill(250);
-  beginShape();
-  vertex(width * .3, height * .32);
-  vertex(width * .38, height * .31);
-  vertex(width * .45, height * .39);
-  vertex(width * .36, height * .42);
-  vertex(width * .3, height * .32);
-  endShape();
-
-  beginShape();
-  curveVertex(width * .7, height * .32);
-  curveVertex(width * .62, height * .31);
-  curveVertex(width * .55, height * .39);
-  curveVertex(width * .64, height * .42);
-  curveVertex(width * .7, height * .32);
-  endShape(CLOSE);
-}
-
-function drawHead() {
-
-  fill(50);
-  beginShape();
-  vertex(width * .05, height * .4);
-  vertex(width * .15, height * .2);
-  vertex(width * .3, height * .2);
-  vertex(width * .45, height * .15);
-  vertex(width * .55, height * .15);
-  vertex(width * .7, height * .2);
-  vertex(width * .85, height * .2);
-  vertex(width * .95, height * .35);
-  vertex(width * .75, height * .3);
-  vertex(width * .72, height * .4);
-  vertex(width * .55, height * .8);
-  vertex(width * .45, height * .8);
-  vertex(width * .28, height * .4);
-  vertex(width * .25, height * .3);
-  endShape();
-
-}
-
-function drawRightpupil() {
-
-  stroke(color(75, 230, 240))
-  strokeWeight(4)
-  fill(120, 230, 240)
-  circle(width * .63, height * .35, 17);
-
-
-}
-
-function drawNosedot() {
-  rectMode(CENTER)
-  noStroke();
-  push();
-  translate(width * .5, height * .79);
-  rotate(Nosesniffle);
-  fill(40)
-  rect(width * 0, height * 0, 15, 20)
-  pop();
-}
-
-function drawNose() {
-  noStroke();
-  beginShape();
-  fill(80)
-  vertex(width * .41, height * .72);
-  vertex(width * .59, height * .72);
-  vertex(width * .55, height * .8);
-  vertex(width * .45, height * .8);
-  endShape();
 }
